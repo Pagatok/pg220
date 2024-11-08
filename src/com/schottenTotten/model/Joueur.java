e12cba6686d043fdb512cb8744c572f235c248d9
Raphaël Kuhn <raphael1.kuhn@gmail.com>
2024-11-08 18:43:39 +0100

    Début de la classe joueur

--- src/com/schottenTotten/model/Joueur.java
+++ src/com/schottenTotten/model/Joueur.java
@@ -1,30 +1,39 @@
 package com.schottenTotten.model;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class Joueur {
+public class Joueur{
     
     private int id_joueur;
-    private final List<Carte> pied;
+    private Card_list pied;
+    private int taille_pied;
+    private static int taille_max_main = 6;
 
     public Joueur(int id_joueur){
         this.id_joueur = id_joueur;
-        this.pied = new ArrayList<>();
+        this.pied = new Card_list(taille_max_main);
+        this.taille_pied = 0;
     }
 
     public int getId(){
         return this.id_joueur;
     }
 
+    public int getTaillePied(){
+        return this.taille_pied;
+    }
+
     public void setId(int id_joueur){
         this.id_joueur = id_joueur;
     }
 
-
-
-    public List<Carte> getCartes() {
-        return new ArrayList<>(pied); // Retourne une copie pour protéger la liste originale
+    public void ajouterCarte(Carte new_carte){
+        if(getTaillePied() < taille_max_main){
+            pied.ajouterCarte(new_carte);
+            this.taille_pied = this.taille_pied + 1;
+        }
+        else{
+            System.out.println("La main ne peut contenir que " + taille_max_main + " cartes maximum");
+        }
     }
 
     
